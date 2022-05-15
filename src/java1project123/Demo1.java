ea68198dc6970b86129ca326cce0ea134cc86ff5
siddhesh <siddheshbhoite94@gmail.com>
2022-05-15 16:12:53 +0530

    first comit forDemo1

---
+++ src/java1project123/Demo1.java
@@ -0,0 +1,15 @@
+package java1project123;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		System.out.println("real madrid");
+		
+		System.out.println("barcelona");
+		
+		
+		
+
+	}
+
+}
